15dc7815ce7bd0a2b1b7fa1209f6de983a928af3
n039727 <n039727@VDIN40061.ads.westernpower.com.au>
2017-04-04 18:31:08 +0530

    Updated service signature.

--- p6-portlet-service/src/main/java/au/com/wp/corp/p6/businessservice/P6SchedulingserviceImpl.java
+++ p6-portlet-service/src/main/java/au/com/wp/corp/p6/businessservice/P6SchedulingserviceImpl.java
@@ -1,7 +1,11 @@
 package au.com.wp.corp.p6.businessservice;
 
+import java.sql.Date;
 import java.sql.Time;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -19,12 +23,38 @@ public class P6SchedulingserviceImpl implements P6SchedulingService{
 	}*/
 	
 	public List<WorkOrder> retrieveWorkOrders(WorkOrderSerachInput input){
+		List<WorkOrder>  workOrders = new ArrayList<WorkOrder>();
 		
-		WorkOrder workOrder = new WorkOrder();
+		WorkOrder workOrder1 = new WorkOrder();
+		workOrder1.setExecutionPackage("test execution 1");
+		workOrder1.setLeadCrew("MOST1");
+		workOrder1.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+		workOrder1.setWorkOrders(Arrays.asList(new String[] {
+				"Y6UIOP67" ,"Y6UIOP70" ,"Y6UIOP97"
+		}));
 		
+		workOrders.add(workOrder1);
+		WorkOrder workOrder2 = new WorkOrder();
+		workOrder2.setExecutionPackage("test execution 1");
+		workOrder2.setLeadCrew("MOST1");
+		workOrder2.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+		workOrder2.setWorkOrders(Arrays.asList(new String[] {
+				"Y6UIOP67" ,"Y6UIOP70" ,"Y6UIOP97"
+		}));
 		
+		workOrders.add(workOrder2);
 		
-		return null;
+		WorkOrder workOrder3 = new WorkOrder();
+		workOrder3.setExecutionPackage("test execution 1");
+		workOrder3.setLeadCrew("MOST1");
+		workOrder3.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+		workOrder3.setWorkOrders(Arrays.asList(new String[] {
+				"Y6UIOP67" ,"Y6UIOP70" ,"Y6UIOP97"
+		}));
+		
+		workOrders.add(workOrder3);
+		
+		return workOrders;
 		
 	}
 
--- p6-portlet-service/src/main/java/au/com/wp/corp/p6/service/MainController.java
+++ p6-portlet-service/src/main/java/au/com/wp/corp/p6/service/MainController.java
@@ -1,8 +1,12 @@
 package au.com.wp.corp.p6.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +29,12 @@ public class MainController {
         return "This is great !!! " +welcomeservice.getWelcomeMessage();
     }
 
-  
+    @RequestMapping(value = "/retrieveWorkOrders" , 
+    		method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
+    @ResponseBody
+    List<WorkOrder> retrieveWorkOrders() {
+        return welcomeservice.retrieveWorkOrders(null);
+    }
     
    
 }
