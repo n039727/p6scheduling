9515fd18a138473afe54b56bc20e5f520ea9dca9
Saikat Ganguly <Saikat.Ganguly@westernpower.com.au>
2017-04-05 18:07:37 +0530

    Rest web services to support basic functionality

--- p6-portlet-service/src/main/java/au/com/wp/corp/p6/businessservice/impl/P6SchedulingBusinessServiceImpl.java
+++ p6-portlet-service/src/main/java/au/com/wp/corp/p6/businessservice/impl/P6SchedulingBusinessServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import au.com.wp.corp.p6.businessservice.P6SchedulingBusinessService;
+import au.com.wp.corp.p6.dto.ToDoItems;
 import au.com.wp.corp.p6.dto.WorkOrder;
 import au.com.wp.corp.p6.dto.WorkOrderSerachInput;
 
@@ -19,74 +20,76 @@ public class P6SchedulingBusinessServiceImpl implements P6SchedulingBusinessServ
 
 	Map<String,WorkOrder> mapStorage = null; 
 	
-	public List<WorkOrder> retrieveWorkOrders(WorkOrderSerachInput input){
-		List<WorkOrder>  workOrders = new ArrayList<WorkOrder>();
+	public List<WorkOrder> retrieveWorkOrders(WorkOrderSerachInput input) {
+		List<WorkOrder> workOrders = new ArrayList<WorkOrder>();
 		/* this code will be replaced will the actual P6 Service call */
-		WorkOrder workOrder1 = new WorkOrder();
-		workOrder1.setExecutionPackage("test execution 1");
-		workOrder1.setLeadCrew("MOST1");
-		workOrder1.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
-		workOrder1.setWorkOrders(Arrays.asList(new String[] {
-				"Y6UIOP67" ,"Y6UIOP70" ,"Y6UIOP97"
-		}));
-		
-		workOrders.add(workOrder1);
-		WorkOrder workOrder2 = new WorkOrder();
-		workOrder2.setExecutionPackage("test execution 1");
-		workOrder2.setLeadCrew("MOST1");
-		workOrder2.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
-		workOrder2.setWorkOrders(Arrays.asList(new String[] {
-				"Y6UIOP67" ,"Y6UIOP70" ,"Y6UIOP97"
-		}));
-		
-		workOrders.add(workOrder2);
-		
-		WorkOrder workOrder3 = new WorkOrder();
-		workOrder3.setExecutionPackage("test execution 1");
-		workOrder3.setLeadCrew("MOST1");
-		workOrder3.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
-		workOrder3.setWorkOrders(Arrays.asList(new String[] {
-				"Y6UIOP67" ,"Y6UIOP70" ,"Y6UIOP97"
-		}));
-		
-		workOrders.add(workOrder3);
+		if (mapStorage == null || mapStorage.size() < 1) {
+			WorkOrder workOrder1 = new WorkOrder();
+			workOrder1.setExecutionPackage("test execution 1");
+			workOrder1.setLeadCrew("MOST1");
+			workOrder1.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+			workOrder1.setWorkOrders(Arrays.asList(new String[] { "Y6UIOP67", "Y6UIOP70", "Y6UIOP97" }));
+			
+			ToDoItems toDoItems = new ToDoItems();
+			toDoItems.setToDoName("ENAR");
+			toDoItems.setWorkOrders(workOrder1.getWorkOrders());
+			
+			workOrder1.setToDoItems(Arrays.asList(new ToDoItems[]{toDoItems}));
+			
+			workOrders.add(workOrder1);
+			WorkOrder workOrder2 = new WorkOrder();
+			workOrder2.setExecutionPackage("test execution 1");
+			workOrder2.setLeadCrew("MOST1");
+			workOrder2.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+			workOrder2.setWorkOrders(Arrays.asList(new String[] { "Y6UIOP67", "Y6UIOP70", "Y6UIOP97" }));
+
+			workOrders.add(workOrder2);
+
+			WorkOrder workOrder3 = new WorkOrder();
+			workOrder3.setExecutionPackage("test execution 1");
+			workOrder3.setLeadCrew("MOST1");
+			workOrder3.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+			workOrder3.setWorkOrders(Arrays.asList(new String[] { "Y6UIOP67", "Y6UIOP70", "Y6UIOP97" }));
+
+			workOrders.add(workOrder3);
+		} else {
+			workOrders = new ArrayList<WorkOrder>(mapStorage.values());
+		}
 		/* this code will be replaced will the actual P6 Service call */
 		return workOrders;
-		
+
 	}
 
-	public List<WorkOrder> retrieveJobs(WorkOrderSerachInput input){
-		List<WorkOrder>  workOrders = new ArrayList<WorkOrder>();
-		
+	public List<WorkOrder> retrieveJobs(WorkOrderSerachInput input) {
+		List<WorkOrder> workOrders = new ArrayList<WorkOrder>();
+
 		/* this code will be replaced will the actual P6 Service call */
-		WorkOrder workOrder1 = new WorkOrder();
-		workOrder1.setLeadCrew("MOST1");
-		workOrder1.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
-		workOrder1.setWorkOrders(Arrays.asList(new String[] {
-				"Y6UIOP97"
-		}));
-		
-		workOrders.add(workOrder1);
-		WorkOrder workOrder2 = new WorkOrder();
-		workOrder2.setLeadCrew("MOST2");
-		workOrder2.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
-		workOrder2.setWorkOrders(Arrays.asList(new String[] {
-				"Y6UIOP67"
-		}));
-		
-		workOrders.add(workOrder2);
-		
-		WorkOrder workOrder3 = new WorkOrder();
-		workOrder3.setLeadCrew("MOST3");
-		workOrder3.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
-		workOrder3.setWorkOrders(Arrays.asList(new String[] {
-				"Y6UIOP67"
-		}));
-		/* this code will be replaced will the actual P6 Service call */
-		workOrders.add(workOrder3);
-		
+		if (mapStorage == null || mapStorage.size() < 1) {
+			WorkOrder workOrder1 = new WorkOrder();
+			workOrder1.setLeadCrew("MOST1");
+			workOrder1.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+			workOrder1.setWorkOrders(Arrays.asList(new String[] { "Y6UIOP97" }));
+
+			workOrders.add(workOrder1);
+			WorkOrder workOrder2 = new WorkOrder();
+			workOrder2.setLeadCrew("MOST2");
+			workOrder2.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+			workOrder2.setWorkOrders(Arrays.asList(new String[] { "Y6UIOP67" }));
+
+			workOrders.add(workOrder2);
+
+			WorkOrder workOrder3 = new WorkOrder();
+			workOrder3.setLeadCrew("MOST3");
+			workOrder3.setScheduleDate(String.valueOf(Date.valueOf(LocalDate.now())));
+			workOrder3.setWorkOrders(Arrays.asList(new String[] { "Y6UIOP67" }));
+			/* this code will be replaced will the actual P6 Service call */
+			workOrders.add(workOrder3);
+		} else {
+			workOrders = new ArrayList<WorkOrder>(mapStorage.values());
+		}
+
 		return workOrders;
-		
+
 	}
 
 	public List<WorkOrder> saveWorkOrder(WorkOrder workOrder) {
