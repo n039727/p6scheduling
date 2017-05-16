f580df70f89c8c634f4fdfc140e1161c704ac31f
Kausik Jana <kausik.jana@westernpower.com.au>
2017-05-16 20:13:11 +0800

    remove hard coded user credentials

--- p6-portlet-service/src/main/java/au/com/wp/corp/p6/wsclient/cleint/impl/P6WSClientImpl.java
+++ p6-portlet-service/src/main/java/au/com/wp/corp/p6/wsclient/cleint/impl/P6WSClientImpl.java
@@ -28,7 +28,7 @@ import au.com.wp.corp.p6.wsclient.logging.RequestTrackingId;
  * @version 1.0
  */
 @Service
-@PropertySource ("file:/${properties.dir}/p6portal-wsdl.properties")
+@PropertySource("file:/${properties.dir}/p6portal-wsdl.properties")
 public class P6WSClientImpl implements P6WSClient {
 	private static final Logger logger = LoggerFactory.getLogger(P6WSClientImpl.class);
 
@@ -37,6 +37,18 @@ public class P6WSClientImpl implements P6WSClient {
 
 	@Value("${P6_AUTH_SERVICE_WSDL}")
 	private String authServiceWSDL;
+
+	@Value("${P6_REAUTH_INTERVAL}")
+	private String intervalReAuthenticateP6;
+
+	@Value("${P6_USER_PRINCIPAL}")
+	private String userPrincipal;
+	
+	@Value ("${P6_USER_CREDENTIAL}")
+	private String userCredential;
+	
+	@Value ("${P6_DB_INSTANCE}")
+	private int p6DBInstance;
 
 	/*
 	 * (non-Javadoc)
@@ -49,15 +61,7 @@ public class P6WSClientImpl implements P6WSClient {
 	public List<WorkOrder> searchWorkOrder(ActivitySearchRequest searchRequest) throws P6ServiceException {
 		logger.info("Calling activity service in P6 Webservice ...");
 		final RequestTrackingId trackingId = new RequestTrackingId();
-		long wsAuthCallTimestamp = CacheManager.getWSLoginTimestamp().get(WS_AUTH_SERVICE_CALL_TIME) != null
-				? (Long) CacheManager.getWSLoginTimestamp().get(WS_AUTH_SERVICE_CALL_TIME) : 0;
-		if (CacheManager.getWsHeaders().isEmpty()
-				|| System.currentTimeMillis() - wsAuthCallTimestamp > 2 * 60 * 60 * 1000) {
-			AuthenticationService authService = new AuthenticationService(trackingId, authServiceWSDL);
-			Holder<Boolean> holder = authService.run();
-			logger.debug("Is authentication successfull ??  {} ", holder.value);
-			CacheManager.getWSLoginTimestamp().put(WS_AUTH_SERVICE_CALL_TIME, System.currentTimeMillis());
-		}
+		getAuthenticated(trackingId);
 
 		if (null == searchRequest) {
 			throw new P6ServiceException("NO_SEARCH_CRITERIA_FOUND");
@@ -87,6 +91,13 @@ public class P6WSClientImpl implements P6WSClient {
 			filter.append(searchRequest.getPlannedStartDate() + " 23:59:59', 'yyyy-mm-dd hh24:mi:ss')");
 		}
 
+		if ( null != searchRequest.getWorkOrder() ) {
+			if (filter.length() > 0)
+				filter.append(OR);
+			filter.append("(Id = ");
+			filter.append("'"+searchRequest.getWorkOrder()+"')");
+		}
+		
 		logger.debug("filter criteria for search # {} ", filter.toString());
 
 		final ActivityService activityService = new ActivityService(trackingId, activityServiceWSDL,
@@ -110,6 +121,26 @@ public class P6WSClientImpl implements P6WSClient {
 		}
 
 		return workOrders;
+	}
+
+	/**
+	 * @param trackingId
+	 * @throws P6ServiceException
+	 */
+	private Boolean getAuthenticated(final RequestTrackingId trackingId) throws P6ServiceException {
+		long wsAuthCallTimestamp = CacheManager.getWSLoginTimestamp().get(WS_AUTH_SERVICE_CALL_TIME) != null
+				? (Long) CacheManager.getWSLoginTimestamp().get(WS_AUTH_SERVICE_CALL_TIME) : 0;
+		if (CacheManager.getWsHeaders().isEmpty()
+				|| System.currentTimeMillis() - wsAuthCallTimestamp > 2 * 60 * 60 * 1000) {
+			AuthenticationService authService = new AuthenticationService(trackingId, authServiceWSDL, userPrincipal,userCredential,p6DBInstance);
+			Holder<Boolean> holder = authService.run();
+			logger.debug("Is authentication successfull ??  {} ", holder.value);
+			if (holder.value)
+				CacheManager.getWSLoginTimestamp().put(WS_AUTH_SERVICE_CALL_TIME, System.currentTimeMillis());
+			return holder.value;
+		}
+
+		return false;
 	}
 
 }
