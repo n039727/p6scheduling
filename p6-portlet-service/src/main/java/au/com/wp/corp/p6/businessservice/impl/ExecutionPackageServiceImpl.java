0c701a7052df70157c6c84fb62111630d8253626
Saikat Ganguly <Saikat.Ganguly@westernpower.com.au>
2017-05-23 17:51:27 +0530

    updated execution package service

--- p6-portlet-service/src/main/java/au/com/wp/corp/p6/businessservice/impl/ExecutionPackageServiceImpl.java
+++ p6-portlet-service/src/main/java/au/com/wp/corp/p6/businessservice/impl/ExecutionPackageServiceImpl.java
@@ -6,7 +6,6 @@ package au.com.wp.corp.p6.businessservice.impl;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
