d3394a8a7b22abdea0a25ce7038539cf9ca918c7
Swapan Pramanick <Swapan.Pramanick@westernpower.com.au>
2017-05-25 01:42:56 +0530

    View pages should show all type of to dos

--- p6-portlet-web/src/main/webapp/js/dynamicToDoSetter.js
+++ p6-portlet-web/src/main/webapp/js/dynamicToDoSetter.js
@@ -34,9 +34,7 @@ function dynamicToDoSetterController($scope) {
 	
 	ctrl.$onChanges = function (changes) {
 		console.log('[DEBUG] On changes called');
-		if (changes.currentMap) {
-		  ctrl.init();
-		}
+		ctrl.init();
 	};
 	
 	ctrl.removeToDo = function(todoList, todo) {
