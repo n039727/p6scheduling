ccd6e1edc0af7b749b9106d86565a7d341d036d1
Tirtha Guha <tirtha.guha@westernpower.com.au>
2017-06-14 19:48:22 +0530

    sorting on pland start date

--- p6-portlet-service/src/main/java/au/com/wp/corp/p6/utils/WorkOrderComparator.java
+++ p6-portlet-service/src/main/java/au/com/wp/corp/p6/utils/WorkOrderComparator.java
@@ -3,7 +3,6 @@ package au.com.wp.corp.p6.utils;
 import java.util.Comparator;
 import java.util.Date;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.CompareToBuilder;
 
 import au.com.wp.corp.p6.dto.WorkOrder;
@@ -18,22 +17,7 @@ public class WorkOrderComparator implements Comparator<WorkOrder> {
 		Date scheduledDate2 = dateUtils.toDateFromDD_MM_YYYY(o2.getScheduleDate());
 		CompareToBuilder compareBuilder=  new CompareToBuilder();
 		
-	//	int c;
-	    //c = scheduledDate1.compareTo(scheduledDate2);
-	  //  if (c == 0){
-	    	if((!StringUtils.isEmpty(o1.getExctnPckgName())) && (!StringUtils.isEmpty(o2.getExctnPckgName()))){
-				/*Date executionPkgNam1 = dateUtils.toDateFromYYYY_MM_DD(o1.getExctnPckgName().split("_")[0]);
-				Date executionPkgNam2 = dateUtils.toDateFromYYYY_MM_DD(o2.getExctnPckgName().split("_")[0]);*/
-	    		
-	    		Date executionPkgNam1 = dateUtils.toDateFromDDMMYYYY(o1.getExctnPckgName().substring(0, 8));
-				Date executionPkgNam2 = dateUtils.toDateFromDDMMYYYY(o2.getExctnPckgName().substring(0, 8));
-				compareBuilder.append(executionPkgNam1, executionPkgNam2);
-				compareBuilder.append(o1.getWorkOrderId(), o2.getWorkOrderId());
-				//c = executionPkgNam1.compareTo(executionPkgNam2);
-			}
-	      
-	  //  }
-	    compareBuilder.append(scheduledDate1, scheduledDate2);
+		compareBuilder.append(scheduledDate1, scheduledDate2);
 		return compareBuilder.toComparison();
 	}
 	
