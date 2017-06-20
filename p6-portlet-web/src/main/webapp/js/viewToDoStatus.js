ab68e63fe43529363a1c1aa16bac442135649a62
Sudhir Jijhautiya <Sudhir.Jijhautiya@westernpower.com.au>
2017-06-20 13:05:57 +0530

    removed the commented out the code

--- p6-portlet-web/src/main/webapp/js/viewToDoStatus.js
+++ p6-portlet-web/src/main/webapp/js/viewToDoStatus.js
@@ -10,8 +10,6 @@ function viewToDoStatusController($scope, ModalService, restTemplate, userAccess
 		}
 	}
 
-	//ctrl.successSavedMsg = "";
-	//ctrl.savedMsgVisible = false;
 	ctrl.isAllCompletedStatus = false;
 	console.log('$ctrl.activeContext in view: ' + JSON.stringify(ctrl.activeContext));
 	console.log('data received: ' + JSON.stringify(ctrl.data));
@@ -78,12 +76,6 @@ function viewToDoStatusController($scope, ModalService, restTemplate, userAccess
 				},
 				data: JSON.stringify(wo)
 		};
-		/*		$http(req).then(function (response) {
-			console.log("Received data from server");
-			$scope.fetchedData = response.data;
-			console.log("Data from server: " + JSON.stringify($scope.fetchedData));
-			ctrl.savedMsgVisible = true;
-		});*/
 		restTemplate.callService(req, function (response) {
 			console.log("Received data from server");
 			$scope.fetchedData = response.data;
@@ -133,21 +125,6 @@ function viewToDoStatusController($scope, ModalService, restTemplate, userAccess
 				},
 				data: JSON.stringify(query)
 		};
-		/*	$http(req).then(function (response) {
-			console.log("Received data from server for fetchWOForTODOStatus: " + JSON.stringify(response.data));
-			wo.todoAssignments = [];
-			wo.todoAssignments = response.data.todoAssignments;
-			if (wo.todoAssignments) {
-				for (var i =0; i<wo.todoAssignments.length; i++) {
-					console.log("req by date for fetchWOForTODOStatus: " + JSON.stringify(wo.todoAssignments[i].reqByDate));
-					if(angular.isDefined(wo.todoAssignments[i].reqByDate) && wo.todoAssignments[i].reqByDate !== null && wo.todoAssignments[i].reqByDate !== ""){					
-						wo.todoAssignments[i].reqByDt = new Date(wo.todoAssignments[i].reqByDate);
-					}
-				}
-			}
-			wo.schedulingComment = response.data.schedulingComment;
-			console.log("Work Order after fetch todo: " + JSON.stringify(wo));
-		});*/
 		restTemplate.callService(req, function (response) {
 			console.log("Received data from server for fetchWOForTODOStatus: " + JSON.stringify(response.data));
 			wo.todoAssignments = [];
