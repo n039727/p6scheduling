f2b7f01b9b176c9bbe8cf3c39a48de6adb662af9
n039727 <Saikat.Ganguly@westernpower.com.au>
2017-07-03 14:38:21 +0530

    Code renamed/rmoved for current relase baseline

--- p6-portlet-service/src/main/java/au/com/wp/corp/p6/utils/WorkOrderComparator.java
+++ p6-portlet-service/src/main/java/au/com/wp/corp/p6/utils/WorkOrderComparator.java
@@ -16,11 +16,14 @@ public class WorkOrderComparator implements Comparator<WorkOrder> {
 		Date scheduledDate1 = dateUtils.toDateFromDD_MM_YYYY(o1.getScheduleDate());
 		Date scheduledDate2 = dateUtils.toDateFromDD_MM_YYYY(o2.getScheduleDate());
 		CompareToBuilder compareBuilder = new CompareToBuilder();
-
+		
+		
 
 		if ((!StringUtils.isEmpty(o1.getExctnPckgName())) && 
 				(!StringUtils.isEmpty(o2.getExctnPckgName()))) {
-			compareBuilder.append(o1.getWorkOrderId(), o2.getWorkOrderId());
+			if(o1.getExctnPckgName().equals(o1.getExctnPckgName())){
+				compareBuilder.append(o1.getWorkOrderId(), o2.getWorkOrderId());
+			}
 		}else{
 			compareBuilder.append(o1.getActioned(),o2.getActioned());
 		}
